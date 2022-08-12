955580ec9b0ee890a674280092e269f52b9a9aaa
Yunus Floo <yunus@floo.id>
2022-08-12 09:09:18 +0700

    add customer name

---
+++ database/migrations/2022_08_10_233036_add_customer_name_on_transaction_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->after('store_id', function (Blueprint $table) {
+                $table->string('customer_name')->nullable();
+                $table->bigInteger('customer_id')->nullable();
+            });
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            //
+        });
+    }
+};
